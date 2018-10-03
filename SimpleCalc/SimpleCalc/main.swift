a352a3b3389c5db0e384774e044304a344ce87d6
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-10-03 01:11:21 -0700

    Finish Homework without EC

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,7 +10,82 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        let oper : String = findOperator(args)
+        
+        switch oper {
+        case "+":
+            let int1 : Int! = Int(args[0])
+            let int2 : Int! = Int(args[2])
+            return int1 + int2
+        
+        case "-":
+            let int1 : Int! = Int(args[0])
+            let int2 : Int! = Int(args[2])
+            return int1 - int2
+        
+        case "%":
+            let int1 : Int! = Int(args[0])
+            let int2 : Int! = Int(args[2])
+            return int1 % int2
+
+        case "/":
+            let int1 : Int! = Int(args[0])
+            let int2 : Int! = Int(args[2])
+            return int1 / int2
+            
+        case "*":
+            let int1 : Int! = Int(args[0])
+            let int2 : Int! = Int(args[2])
+            return int1 * int2
+        
+        case "count":
+            if args.count < 2 {
+                return 0
+            } else {
+                return args.count - 1
+            }
+        
+        case "avg":
+            var summed : Int! = 0
+            if args.count < 2 {
+                return 0
+            } else {
+                for i in 0...args.count - 2 {
+                    let toAdd : Int! = Int(args[i])
+                    summed += toAdd
+                }
+                let num : Int = summed / (args.count - 1)
+                return num
+            }
+            
+        case "fact":
+            if args.count < 2 {
+                return 0
+            } else if args[0] == "1" || args[0] == "0" {
+                return 1
+            } else {
+                var product : Int! = Int(args[0])
+                for i in stride(from: product - 1, to: 1, by: -1) {
+                    product *= i
+                }
+                return product
+            }
+            
+        default:
+            print("Error")
+            
+        }
+        return 0
     }
+    
+    private func findOperator(_ args: [String]) -> String {
+        if Int(args[args.count - 1]) != nil {
+            return args[args.count - 2]
+        } else {
+            return args[args.count - 1]
+        }
+    }
+    
     
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
